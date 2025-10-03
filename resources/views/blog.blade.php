ecbcd045f85336d521b62588879ec4c8029a4610
unknown <kamrankhan3.61k@gmail.com>
2025-10-03 09:34:29 +0500

    Blog blade adding the code

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,209 @@
+@extends('layouts.app')
+@section('content')
+<!-- Start Page Header -->
+  <section class="cs_page_heading cs_center cs_bg_filed" data-src="assets/img/page_heading_bg.jpg">
+    <div class="container">
+      <div class="cs_page_heading_in">
+        <h1 class="cs_page_heading_title cs_fs_48 cs_white_color">Blog</h1>
+        <ol class="breadcrumb cs_fs_20 cs_white_color cs_heading_font">
+          <li class="breadcrumb-item"><a href="index.html"><i class="fa-solid fa-house"></i>Home</a></li>
+          <li class="breadcrumb-item active">Blog</li>
+        </ol>
+      </div>
+    </div>
+  </section>
+  <!-- End Page Header -->
+  <!-- Start Blog Section -->
+  <div class="cs_height_120 cs_height_lg_80"></div>
+  <div class="container">
+    <div class="row cs_row_gap_30 cs_gap_y_40">
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_1.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">August 04, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">Medi Tips How to Physical Fitness your Health</a></h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_2.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">August 15, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">How to Get Best Care 7 Step for your Eyes</a>
+            </h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_3.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">September 01, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">Improve your heart health with genetic testing.</a>
+            </h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_13.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">August 04, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">How Recovery & Rehabilitation Work walk hand</a>
+            </h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_14.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">August 15, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">Discover the Purpose of Detox and Start Healing</a>
+            </h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_15.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">September 01, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">Threats for the People who use Drugs Experiences</a>
+            </h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_16.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">August 04, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">Stories of how therapy has helped individuals</a>
+            </h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_17.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">August 15, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">Tips on practicing mindfulness and its benefits.</a>
+            </h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <article class="cs_post cs_style_1">
+          <a href="blog-details.html" class="cs_post_thumbnail">
+            <img src="assets/img/post_18.jpeg" alt="Post Thumbnail">
+          </a>
+          <div class="cs_post_info">
+            <div class="cs_post_meta">September 01, 2024</div>
+            <h3 class="cs_post_title cs_fs_32">
+              <a href="blog-details.html">Latest research and discover in psychology</a>
+            </h3>
+            <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+              <span>Learn More</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </article>
+      </div>
+    </div>
+    <div class="cs_height_50 cs_height_lg_40"></div>
+    <div class="cs_center">
+      <a href="#" class="cs_btn cs_style_1 cs_fs_18 cs_semibold cs_accent_bg  cs_radius_100">
+        <span class="cs_btn_text">See All Blogs</span>
+      </a>
+    </div>
+  </div>
+  <div class="cs_height_120 cs_height_lg_80"></div>
+  <!-- End Blog Section -->
+@endsection
