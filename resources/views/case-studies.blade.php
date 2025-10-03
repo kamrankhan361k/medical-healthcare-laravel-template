420450be3c9d87f5ce25f458a7bf015c20e5def5
unknown <kamrankhan3.61k@gmail.com>
2025-10-03 09:41:11 +0500

    Case-Studies blade adding the code

---
+++ resources/views/case-studies.blade.php
@@ -0,0 +1,142 @@
+@extends('layouts.app')
+@section('content')
+<!-- Start Page Header -->
+  <section class="cs_page_heading cs_center cs_bg_filed" data-src="assets/img/page_heading_bg.jpg">
+    <div class="container">
+      <div class="cs_page_heading_in">
+        <h1 class="cs_page_heading_title cs_fs_48 cs_white_color">Case Studies</h1>
+        <ol class="breadcrumb cs_fs_20 cs_white_color cs_heading_font">
+          <li class="breadcrumb-item"><a href="index.html"><i class="fa-solid fa-house"></i>Home</a></li>
+          <li class="breadcrumb-item active">Case Studies</li>
+        </ol>
+      </div>
+    </div>
+  </section>
+  <!-- End Page Header -->
+  <!-- Start Blog Section -->
+  <div class="cs_height_120 cs_height_lg_80"></div>
+  <div class="container">
+    <div class="row cs_row_gap_30 cs_gap_y_30">
+      <div class="col-lg-4 col-md-6">
+        <div class="cs_card cs_style_1 cs_radius_10">
+          <a href="case-studies-details.html" class="cs_card_thumbnail cs_radius_10">
+            <img src="assets/img/casestydy_4.jpeg" alt="Card Thumbnail">
+          </a>
+          <div class="cs_card_bio">
+            <p class="cs_card_subtitle cs_fs_14 cs_heading_color">Eye Care</p>
+            <h3 class="cs_card_title cs_fs_24 cs_bold"><a href="case-studies-details.html">Eye Care Services</a>
+            </h3>
+            <a href="case-studies-details.html" class="cs_card_btn">
+              <span>Learn More</span>
+              <div class="cs_card_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="cs_card cs_style_1 cs_radius_10">
+          <a href="case-studies-details.html" class="cs_card_thumbnail cs_radius_10">
+            <img src="assets/img/casestydy_2.jpeg" alt="Card Thumbnail">
+          </a>
+          <div class="cs_card_bio">
+            <p class="cs_card_subtitle cs_fs_14 cs_heading_color">Gynology Operation</p>
+            <h3 class="cs_card_title cs_fs_24 cs_bold"><a href="case-studies-details.html">Two Baby cesar opr.</a>
+            </h3>
+            <a href="case-studies-details.html" class="cs_card_btn">
+              <span>Learn More</span>
+              <div class="cs_card_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="cs_card cs_style_1 cs_radius_10">
+          <a href="case-studies-details.html" class="cs_card_thumbnail cs_radius_10">
+            <img src="assets/img/casestydy_5.jpeg" alt="Card Thumbnail">
+          </a>
+          <div class="cs_card_bio">
+            <p class="cs_card_subtitle cs_fs_14 cs_heading_color">Teth Department</p>
+            <h3 class="cs_card_title cs_fs_24 cs_bold"><a href="case-studies-details.html">Dental Surgery</a></h3>
+            <a href="case-studies-details.html" class="cs_card_btn">
+              <span>Learn More</span>
+              <div class="cs_card_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="cs_card cs_style_1 cs_radius_10">
+          <a href="case-studies-details.html" class="cs_card_thumbnail cs_radius_10">
+            <img src="assets/img/casestydy_1.jpeg" alt="Card Thumbnail">
+          </a>
+          <div class="cs_card_bio">
+            <p class="cs_card_subtitle cs_fs_14 cs_heading_color">Personal Consulting</p>
+            <h3 class="cs_card_title cs_fs_24 cs_bold"><a href="case-studies-details.html">Supports Patients</a>
+            </h3>
+            <a href="case-studies-details.html" class="cs_card_btn">
+              <span>Learn More</span>
+              <div class="cs_card_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="cs_card cs_style_1 cs_radius_10">
+          <a href="case-studies-details.html" class="cs_card_thumbnail cs_radius_10">
+            <img src="assets/img/casestydy_6.jpeg" alt="Card Thumbnail">
+          </a>
+          <div class="cs_card_bio">
+            <p class="cs_card_subtitle cs_fs_14 cs_heading_color">Gynology</p>
+            <h3 class="cs_card_title cs_fs_24 cs_bold"><a href="case-studies-details.html">Pregnancy Care Advice</a>
+            </h3>
+            <a href="case-studies-details.html" class="cs_card_btn">
+              <span>Learn More</span>
+              <div class="cs_card_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="cs_card cs_style_1 cs_radius_10">
+          <a href="case-studies-details.html" class="cs_card_thumbnail cs_radius_10">
+            <img src="assets/img/casestydy_3.jpeg" alt="Card Thumbnail">
+          </a>
+          <div class="cs_card_bio">
+            <p class="cs_card_subtitle cs_fs_14 cs_heading_color">Heart Department</p>
+            <h3 class="cs_card_title cs_fs_24 cs_bold"><a href="case-studies-details.html">Heart Surgery</a></h3>
+            <a href="case-studies-details.html" class="cs_card_btn">
+              <span>Learn More</span>
+              <div class="cs_card_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+            <hr>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="cs_height_120 cs_height_lg_80"></div>
+  <!-- End Blog Section -->
+@endsection
