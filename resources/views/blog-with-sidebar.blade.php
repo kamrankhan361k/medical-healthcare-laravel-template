a665d16f7286a2cea07fc6816aba50a056aa0ed5
unknown <kamrankhan3.61k@gmail.com>
2025-10-03 09:32:00 +0500

    Blog-with-sidebar blade adding the code

---
+++ resources/views/blog-with-sidebar.blade.php
@@ -0,0 +1,209 @@
+@extends('layouts.app')
+@section('content')
+<!-- Start Page Header -->
+  <section class="cs_page_heading cs_center cs_bg_filed" data-src="assets/img/page_heading_bg.jpg">
+    <div class="container">
+      <div class="cs_page_heading_in">
+        <h1 class="cs_page_heading_title cs_fs_48 cs_white_color">Blog With Sidebar</h1>
+        <ol class="breadcrumb cs_fs_20 cs_white_color cs_heading_font">
+          <li class="breadcrumb-item"><a href="index.html"><i class="fa-solid fa-house"></i>Home</a></li>
+          <li class="breadcrumb-item active">Blog</li>
+        </ol>
+      </div>
+    </div>
+  </section>
+  <!-- End Page Header -->
+  <!-- Start Blog Section -->
+  <div class="cs_height_120 cs_height_lg_80"></div>
+  <div class="container">
+    <div class="row cs_gap_y_40">
+      <div class="col-lg-8">
+        <div class="row cs_row_gap_30 cs_gap_y_30">
+          <div class="col-lg-6">
+            <article class="cs_post cs_style_1 cs_type_1 cs_radius_10">
+              <a href="blog-details.html" class="cs_post_thumbnail">
+                <img src="assets/img/post_1.jpeg" alt="Post Thumbnail">
+              </a>
+              <div class="cs_post_info">
+                <div class="cs_post_meta">August 04, 2024</div>
+                <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">Medi Tips How to Physical Fitness your Health</a></h3>
+                <a href="blog-details.html" class="cs_text_btn cs_type_1 cs_fs_18 cs_heading_color">
+                  <span>Learn More</span>
+                  <div class="cs_text_btn_icon cs_heading_bg cs_white_color cs_radius_50 cs_center">
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  </div>
+                </a>
+              </div>
+            </article>
+          </div>
+          <div class="col-lg-6">
+            <article class="cs_post cs_style_1 cs_type_1 cs_radius_10">
+              <a href="blog-details.html" class="cs_post_thumbnail">
+                <img src="assets/img/post_2.jpeg" alt="Post Thumbnail">
+              </a>
+              <div class="cs_post_info">
+                <div class="cs_post_meta">August 15, 2024</div>
+                <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">How to Get Best Care 7 Step for your Eyes</a></h3>
+                <a href="blog-details.html" class="cs_text_btn cs_type_1 cs_fs_18 cs_heading_color">
+                  <span>Learn More</span>
+                  <div class="cs_text_btn_icon cs_heading_bg cs_white_color cs_radius_50 cs_center">
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  </div>
+                </a>
+              </div>
+            </article>
+          </div>
+          <div class="col-lg-6">
+            <article class="cs_post cs_style_1 cs_type_1 cs_radius_10">
+              <a href="blog-details.html" class="cs_post_thumbnail">
+                <img src="assets/img/post_3.jpeg" alt="Post Thumbnail">
+              </a>
+              <div class="cs_post_info">
+                <div class="cs_post_meta">September 01, 2024</div>
+                <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">Improve your heart health with genetic testing.</a></h3>
+                <a href="blog-details.html" class="cs_text_btn cs_type_1 cs_fs_18 cs_heading_color">
+                  <span>Learn More</span>
+                  <div class="cs_text_btn_icon cs_heading_bg cs_white_color cs_radius_50 cs_center">
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  </div>
+                </a>
+              </div>
+            </article>
+          </div>
+          <div class="col-lg-6">
+            <article class="cs_post cs_style_1 cs_type_1 cs_radius_10">
+              <a href="blog-details.html" class="cs_post_thumbnail">
+                <img src="assets/img/post_13.jpeg" alt="Post Thumbnail">
+              </a>
+              <div class="cs_post_info">
+                <div class="cs_post_meta">August 04, 2024</div>
+                <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">How Recovery & Rehabilitation Work walk hand</a></h3>
+                <a href="blog-details.html" class="cs_text_btn cs_type_1 cs_fs_18 cs_heading_color">
+                  <span>Learn More</span>
+                  <div class="cs_text_btn_icon cs_heading_bg cs_white_color cs_radius_50 cs_center">
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  </div>
+                </a>
+              </div>
+            </article>
+          </div>
+          <div class="col-lg-6">
+            <article class="cs_post cs_style_1 cs_type_1 cs_radius_10">
+              <a href="blog-details.html" class="cs_post_thumbnail">
+                <img src="assets/img/post_14.jpeg" alt="Post Thumbnail">
+              </a>
+              <div class="cs_post_info">
+                <div class="cs_post_meta">August 15, 2024</div>
+                <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">Discover the Purpose of Detox and Start Healing</a></h3>
+                <a href="blog-details.html" class="cs_text_btn cs_type_1 cs_fs_18 cs_heading_color">
+                  <span>Learn More</span>
+                  <div class="cs_text_btn_icon cs_heading_bg cs_white_color cs_radius_50 cs_center">
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  </div>
+                </a>
+              </div>
+            </article>
+          </div>
+          <div class="col-lg-6">
+            <article class="cs_post cs_style_1 cs_type_1 cs_radius_10">
+              <a href="blog-details.html" class="cs_post_thumbnail">
+                <img src="assets/img/post_15.jpeg" alt="Post Thumbnail">
+              </a>
+              <div class="cs_post_info">
+                <div class="cs_post_meta">September 01, 2024</div>
+                <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">Threats for the People who use Drugs Experiences</a></h3>
+                <a href="blog-details.html" class="cs_text_btn cs_type_1 cs_fs_18 cs_heading_color">
+                  <span>Learn More</span>
+                  <div class="cs_text_btn_icon cs_heading_bg cs_white_color cs_radius_50 cs_center">
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                    <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  </div>
+                </a>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+      <aside class="col-lg-4">
+        <div class="cs_sidebar cs_style_1">
+          <div class="cs_sidebar_widget cs_gray_bg cs_radius_10">
+            <h3 class="cs_sidebar_title cs_fs_20 cs_semibold cs_accent_color">Search</h3>
+            <form action="#" class="cs_search cs_radius_6">
+              <span class="cs_search_icon cs_accent_color"><i class="fa-solid fa-magnifying-glass"></i></span>
+              <input type="text" placeholder="Search....">
+            </form>
+          </div>
+          <div class="cs_sidebar_widget cs_gray_bg cs_radius_10">
+            <h3 class="cs_sidebar_title cs_fs_20 cs_semibold cs_accent_color">Categories</h3>
+            <ul class="cs_categories cs_accent_color cs_mp0">
+              <li><a href="#">Medical</a><a href="#">(18)</a></li>
+              <li><a href="#">Eye Care</a><a href="#">(5)</a></li>
+              <li><a href="#">Dental</a><a href="#">(2)</a></li>
+              <li><a href="#">Consulting</a><a href="#">(11)</a></li>
+              <li><a href="#">Council Rehab</a><a href="#">(4)</a></li>
+              <li><a href="#">Hospitality</a><a href="#">(8)</a></li>
+            </ul>
+          </div>
+          <div class="cs_sidebar_widget cs_gray_bg cs_radius_10">
+            <h3 class="cs_sidebar_title cs_fs_20 cs_semibold cs_accent_color">Recent Posts</h3>
+            <div class="cs_post cs_style_3">
+              <a href="blog-details-right-sidebar.html" class="cs_post_thumbnail cs_radius_10">
+                <img src="assets/img/latest_post_1.jpeg" alt="Image" class="cs_zoom_in">
+              </a>
+              <div class="cs_post_info">
+                <h3 class="cs_post_title cs_fs_18 cs_semibold cs_heading_color"><a href="blog-details.html">Planning your online business goals with a specialists.</a>
+                </h3>
+                <div class="cs_post_meta cs_fs_14 cs_accent_color">
+                  26 Sep 2024</div>
+              </div>
+            </div>
+            <div class="cs_post cs_style_3">
+              <a href="blog-details-right-sidebar.html" class="cs_post_thumbnail cs_radius_10">
+                <img src="assets/img/latest_post_2.jpeg" alt="Image" class="cs_zoom_in">
+              </a>
+              <div class="cs_post_info">
+                <h3 class="cs_post_title cs_fs_18 cs_semibold cs_heading_color">
+                  <a href="blog-details.html">Boost your startup business with our digital agency.</a>
+                </h3>
+                <div class="cs_post_meta cs_fs_14 cs_accent_color">26 Aug 2024</div>
+              </div>
+            </div>
+            <div class="cs_post cs_style_3">
+              <a href="blog-details-right-sidebar.html" class="cs_post_thumbnail cs_radius_10">
+                <img src="assets/img/latest_post_3.jpeg" alt="Image">
+              </a>
+              <div class="cs_post_info">
+                <h3 class="cs_post_title cs_fs_18 cs_semibold cs_heading_color">
+                  <a href="blog-details.html">The importance of a strong digital for strat.</a>
+                </h3>
+                <div class="cs_post_meta cs_fs_14 cs_accent_color">15 Aug 2024</div>
+              </div>
+            </div>
+          </div>
+          <div class="cs_sidebar_widget cs_gray_bg cs_radius_10">
+            <div class="cs_sidebar_tags">
+              <h3 class="cs_sidebar_title cs_fs_20 cs_semibold cs_accent_color">Tags</h3>
+              <div class="cs_tag_list cs_fs_14 cs_heading_font cs_accent_color">
+                <a href="#" class="cs_tag_link cs_white_bg cs_radius_6">Medical</a>
+                <a href="#" class="cs_tag_link cs_white_bg cs_radius_6">Rehab</a>
+                <a href="#" class="cs_tag_link cs_white_bg cs_radius_6">Psychology</a>
+                <a href="#" class="cs_tag_link cs_white_bg cs_radius_6">Eyecare</a>
+                <a href="#" class="cs_tag_link cs_white_bg cs_radius_6">Dental</a>
+                <a href="#" class="cs_tag_link cs_white_bg cs_radius_6">Phytotherapy</a>
+                <a href="#" class="cs_tag_link cs_white_bg cs_radius_6">Hospitality</a>
+                <a href="#" class="cs_tag_link cs_white_bg cs_radius_6">Senior Care</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </aside>
+    </div>
+  </div>
+  <div class="cs_height_120 cs_height_lg_80"></div>
+  <!-- End Blog Section -->
+@endsection
