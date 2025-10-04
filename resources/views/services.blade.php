05b1986fdf2783fd67685b8828c56628921b34f9
unknown <kamrankhan3.61k@gmail.com>
2025-10-04 14:33:36 +0500

    service adding the code

---
+++ resources/views/services.blade.php
@@ -0,0 +1,391 @@
+@extends('layouts.app')
+@section('content')
+  <!-- Start Page Header -->
+  <section class="cs_page_heading cs_center cs_bg_filed" data-src="assets/img/page_heading_bg.jpg">
+    <div class="container">
+      <div class="cs_page_heading_in">
+        <h1 class="cs_page_heading_title cs_fs_48 cs_white_color">Services</h1>
+        <ol class="breadcrumb cs_fs_20 cs_white_color cs_heading_font">
+          <li class="breadcrumb-item"><a href="index.html"><i class="fa-solid fa-house"></i>Home</a></li>
+          <li class="breadcrumb-item active">Services</li>
+        </ol>
+      </div>
+    </div>
+  </section>
+  <!-- End Page Header -->
+  <!-- Start Service Section -->
+  <section class="cs_service_area cs_type_3">
+    <div class="cs_height_120 cs_height_lg_80"></div>
+    <div class="container">
+      <div class="cs_section_heading cs_style_1 cs_type_1">
+        <div class="cs_section_heading_left">
+          <p class="cs_section_subtitle cs_fs_18 cs_semibold cs_accent_color cs_heading_font">Expertise</p>
+          <h2 class="cs_section_title cs_accent_color mb-0">We offer more than Services & all Solutions Medical.</h2>
+        </div>
+        <div class="cs_section_heading_right cs_accent_color text-end">the other hand, we denounce with righteous
+          indignation and dislike men who are so beguiled and demoralized</div>
+      </div>
+      <div class="cs_height_100 cs_height_lg_40"></div>
+      <div class="row cs_gap_y_30">
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_4.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Chiropractic Care</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_5.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Acupuncture Therapy</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_6.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Message Therapy</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_7.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Laser Therapy</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_8.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Sports Injury Therapy</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_9.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Join Mobilization</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_5.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Acupuncture Therapy</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_6.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Message Therapy</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="cs_iconbox cs_style_8 cs_white_bg">
+            <div class="cs_iconbox_icon cs_center cs_radius_50">
+              <img src="assets/img/icons/service_icon_7.svg" alt="Icon">
+            </div>
+            <h3 class="cs_iconbox_title cs_fs_32 cs_semibold">
+              <a href="service-details.html">Laser Therapy</a>
+            </h3>
+            <p class="cs_iconbox_subtitle">
+              The other hand, we denounce with righteous Indignation and dislike men who</p>
+            <a href="service-details.html" class="cs_text_btn cs_fs_18 cs_semibold cs_heading_color">
+              <span>Get Services</span>
+              <div class="cs_text_btn_icon cs_center">
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                <span><i class="fa-solid fa-arrow-right-long"></i></span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cs_height_120 cs_height_lg_80"></div>
+  </section>
+  <!-- End Service Section -->
+  <!-- Start Appointment Section -->
+  <section class="cs_accent_bg">
+    <div class="cs_height_120 cs_height_lg_80"></div>
+    <div class="container">
+      <div class="row cs_gap_y_40">
+        <div class="col-lg-6">
+          <div class="cs_section_heading cs_style_1">
+            <p class="cs_section_subtitle cs_white_color">Appointment</p>
+            <h2 class="cs_section_title cs_fs_48 cs_white_color mb-0">Get Amazing Treatment For our Specialist Doctors
+            </h2>
+          </div>
+          <div class="cs_height_50 cs_height_lg_40"></div>
+          <div class="row cs_gap_y_40">
+            <div class="col-12">
+              <div class="cs_iconbox cs_style_3">
+                <div class="cs_iconbox_icon cs_center">
+                  <img src="assets/img/icons/security_icon.svg" alt="Icon">
+                </div>
+                <div class="cs_iconbox_text">
+                  <h3 class="cs_iconbox_title cs_fs_32 cs_white_color">100% Safe and Trusted</h3>
+                  <p class="cs_iconbox_subtitle cs_white_color mb-0">Far far away, behind the word bore et dolore magna
+                    aliqua. Ut enim ad on minim veniam, quis nostrud</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="cs_iconbox cs_style_3">
+                <div class="cs_iconbox_icon cs_center">
+                  <img src="assets/img/icons/surgery_icon.svg" alt="Icon">
+                </div>
+                <div class="cs_iconbox_text">
+                  <h3 class="cs_iconbox_title cs_fs_32 cs_white_color">Specialist eye Surgery</h3>
+                  <p class="cs_iconbox_subtitle cs_white_color mb-0">Far far away, behind the word bore et dolore magna
+                    aliqua. Ut enim ad on minim veniam, quis nostrud</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="cs_iconbox cs_style_3">
+                <div class="cs_iconbox_icon cs_center">
+                  <img src="assets/img/icons/support_icon.svg" alt="Icon">
+                </div>
+                <div class="cs_iconbox_text">
+                  <h3 class="cs_iconbox_title cs_fs_32 cs_white_color">24/7 Take care Staff</h3>
+                  <p class="cs_iconbox_subtitle cs_white_color mb-0">Far far away, behind the word bore et dolore magna
+                    aliqua. Ut enim ad on minim veniam, quis nostrud</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="cs_appointment_form_wrapper cs_type_1 cs_radius_10">
+            <h3 class="cs_appointment_heading">Make an Appointment</h3>
+            <form class="cs_appointment_form row cs_gap_y_30">
+              <div class="col-md-6">
+                <div class="cs_form_field_wrapper cs_radius_10">
+                  <input type="text" name="name" class="cs_form_field cs_fs_14" placeholder="Your Name">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="cs_form_field_wrapper cs_radius_10">
+                  <input type="text" name="phone" class="cs_form_field cs_fs_14" placeholder="Phone Number">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="cs_form_field_wrapper cs_radius_8">
+                  <select name="department" class="cs_form_field_wrapper cs_radius_8 cs_select"
+                    data-placeholder="Select Department">
+                    <option value="dental">Dental</option>
+                    <option value="pediatric">Pediatric</option>
+                    <option value="gynecology">Gynecology</option>
+                    <option value="cardiology">Cardiology</option>
+                    <option value="neurology">Neurology</option>
+                    <option value="psychiatry">Psychiatry</option>
+                    <option value="others">Others</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="cs_form_field_wrapper cs_radius_10">
+                  <input type="text" name="date" class="cs_form_field" id="datepicker" placeholder="DD MM  YY">
+                  <span class="cs_input_icon position-absolute"><i class="fa-regular fa-calendar-days"></i></span>
+                </div>
+              </div>
+              <div class="col-md-12">
+                <div class="cs_form_field_wrapper cs_radius_10">
+                  <textarea name="Message" rows="6" class="cs_form_field cs_fs_14"
+                    placeholder="Write Message"></textarea>
+                </div>
+              </div>
+              <div class="col-md-12">
+                <button type="submit" class="cs_btn cs_style_1 cs_fs_18 cs_semibold cs_accent_bg  cs_radius_100">
+                  <span class="cs_btn_text">Book An Appointment</span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cs_height_120 cs_height_lg_80"></div>
+  </section>
+  <!-- End Appointment Section -->
+  <!-- Start Blog Section -->
+  <section>
+    <div class="cs_height_120 cs_height_lg_80"></div>
+    <div class="container">
+      <div class="cs_section_heading cs_style_1 cs_type_1">
+        <div class="cs_section_heading_left">
+          <p class="cs_section_subtitle cs_accent_color cs_fs_18 cs_semibold cs_heading_font">Blog & news</p>
+          <h2 class="cs_section_title cs_fs_48 mb-0">Take a Look at The Latest <br> Articles & News</h2>
+        </div>
+        <div class="cs_section_heading_right">
+          <a href="blog.html" class="cs_btn cs_style_1 cs_fs_18 cs_semibold cs_accent_bg  cs_radius_100">
+            <span class="cs_btn_text">See All Blogs</span>
+          </a>
+        </div>
+      </div>
+      <div class="cs_height_50 cs_height_lg_40"></div>
+      <div class="row cs_row_gap_30 cs_gap_y_40">
+        <div class="col-lg-4">
+          <article class="cs_post cs_style_1">
+            <a href="blog-details.html" class="cs_post_thumbnail">
+              <img src="assets/img/post_1.jpeg" alt="Post Thumbnail">
+            </a>
+            <div class="cs_post_info">
+              <div class="cs_post_meta">August 04, 2024</div>
+              <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">
+                  Medi Tips How to Physical Fitness your Health</a>
+              </h3>
+              <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+                <span>Learn More</span>
+                <div class="cs_text_btn_icon cs_center">
+                  <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                </div>
+              </a>
+            </div>
+          </article>
+        </div>
+        <div class="col-lg-4">
+          <article class="cs_post cs_style_1">
+            <a href="blog-details.html" class="cs_post_thumbnail">
+              <img src="assets/img/post_2.jpeg" alt="Post Thumbnail">
+            </a>
+            <div class="cs_post_info">
+              <div class="cs_post_meta">August 15, 2024</div>
+              <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">
+                  How to Get Best Care 7 Step for your Eyes</a>
+              </h3>
+              <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+                <span>Learn More</span>
+                <div class="cs_text_btn_icon cs_center">
+                  <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                </div>
+              </a>
+            </div>
+          </article>
+        </div>
+        <div class="col-lg-4">
+          <article class="cs_post cs_style_1">
+            <a href="blog-details.html" class="cs_post_thumbnail">
+              <img src="assets/img/post_3.jpeg" alt="Post Thumbnail">
+            </a>
+            <div class="cs_post_info">
+              <div class="cs_post_meta">September 01, 2024</div>
+              <h3 class="cs_post_title cs_fs_32"><a href="blog-details.html">Improve your heart health with genetic
+                  testing.</a></h3>
+              <a href="blog-details.html" class="cs_text_btn cs_fs_18 cs_heading_color">
+                <span>Learn More</span>
+                <div class="cs_text_btn_icon cs_center">
+                  <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                  <span><i class="fa-solid fa-arrow-right-long"></i></span>
+                </div>
+              </a>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+    <div class="cs_height_120 cs_height_lg_80"></div>
+  </section>
+  <!-- End Blog Section -->
+@endsection
