7698fcd650775ac60d95cdffae571968a3d4782e
unknown <kamrankhan3.61k@gmail.com>
2025-10-04 14:29:19 +0500

    product adding the code

---
+++ resources/views/products.blade.php
@@ -0,0 +1,133 @@
+@extends('layouts.app')
+@section('content')
+    <!-- Start Page Header -->
+    <section class="cs_page_heading cs_center cs_bg_filed" data-src="assets/img/page_heading_bg.jpg">
+        <div class="container">
+            <div class="cs_page_heading_in">
+                <h1 class="cs_page_heading_title cs_fs_48 cs_white_color">Products</h1>
+                <ol class="breadcrumb cs_fs_20 cs_white_color cs_heading_font">
+                    <li class="breadcrumb-item"><a href="index.html"><i class="fa-solid fa-house"></i>Home</a></li>
+                    <li class="breadcrumb-item active">Products</li>
+                </ol>
+            </div>
+        </div>
+    </section>
+    <!-- End Page Header -->
+    <!-- Start Products Section -->
+    <div class="cs_height_120 cs_height_lg_80"></div>
+    <div class="container">
+        <div class="row cs_row_gap_30 cs_gap_y_30">
+            <div class="col-lg-4 col-md-6">
+                <div class="cs_card cs_style_3">
+                    <div class="cs_card_img cs_radius_10">
+                        <img src="assets/img/product_img_1.jpeg" alt="Product Image">
+                    </div>
+                    <div class="cs_card_info">
+                        <h3 class="cs_card_title cs_fs_24"><a href="#">Medical Mask</a></h3>
+                        <p class="cs_card_subtitle cs_fs_16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="cs_price_box">
+                            <span class="cs_price">$29.00</span>
+                            <a href="#" class="cs_btn cs_style_3 cs_primary_color cs_radius_5">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="cs_card cs  _style_3">
+                    <div class="cs_card_img cs_radius_10">
+                        <img src="assets/img/product_img_2.jpeg" alt="Product Image">
+                    </div>
+                    <div class="cs_card_info">
+                        <h3 class="cs_card_title cs_fs_24"><a href="#">Medical Gloves</a></h3>
+                        <p class="cs_card_subtitle cs_fs_16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="cs_price_box">
+                            <span class="cs_price">$39.00</span>
+                            <a href="#" class="cs_btn cs_style_3 cs_primary_color cs_radius_5">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="cs_card cs_style_3">
+                    <div class="cs_card_img cs_radius_10">
+                        <img src="assets/img/product_img_3.jpeg" alt="Product Image">
+                    </div>
+                    <div class="cs_card_info">
+                        <h3 class="cs_card_title cs_fs_24"><a href="#">Hand Sanitizer</a></h3>
+                        <p class="cs_card_subtitle cs_fs_16 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                            do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="cs_price_box">
+                            <span class="cs_price">$49.00</span>
+                            <a href="#" class="cs_btn cs_style_3 cs_primary_color cs_radius_5">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="cs_card cs_style_3">
+                    <div class="cs_card_img cs_radius_10">
+                        <img src="assets/img/product_img_4.jpeg" alt="Product Image">
+                    </div>
+                    <div class="cs_card_info">
+                        <h3 class="cs_card_title cs_fs_24"><a href="#">Medical Mask
+                            </a></h3>
+                        <p class="cs_card_subtitle cs_fs_16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="cs_price_box">
+                            <span class="cs_price">$29.00</span>
+                            <a href="#" class="cs_btn cs_style_3 cs_primary_color cs_radius_ 5">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="cs_card cs_style_3">
+                    <div class="cs_card_img cs_radius_10">
+                        <img src="assets/img/product_img_5.jpeg" alt="Product Image">
+                    </div>
+                    <div class="cs_card_info">
+                        <h3 class="cs_card_title cs_fs_24"><a href="#">Medical Gloves</a></h3>
+                        <p class="cs_card_subtitle cs_fs_16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="cs_price_box">
+                            <span class="cs_price">$39.00</span>
+                            <a href="#" class="cs_btn cs_style_3 cs_primary_color cs_radius_5">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="cs_card cs_style_3">
+                    <div class="cs_card_img cs_radius_10">
+                        <img src="assets/img/product_img_6.jpeg" alt="Product Image">
+                    </div>
+                    <div class="cs_card_info">
+                        <h3 class="cs_card_title cs_fs_24"><a href="#">Hand Sanitizer</a></h3>
+                        <p class="cs_card_subtitle cs_fs_16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="cs_price_box">
+                            <span class="cs_price">$49.00</span>
+                            <a href="#" class="cs_btn cs_style_3 cs_primary_color cs_radius_5">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cs_height_120 cs_height_lg_80"></div>
+    <!-- End Products Section -->
+    <!-- Start Gallery Section -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="cs_text_center">
+                    <a href="#" class="cs_btn cs_style_2 cs_primary_color cs_radius_5">Load More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cs_height_120 cs_height_lg_80"></div>
+    <!-- End Gallery Section -->
+@endsection
