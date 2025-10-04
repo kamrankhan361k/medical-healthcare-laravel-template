42c555739e129234b3b4f516fa4ff96c0a889dcf
unknown <kamrankhan3.61k@gmail.com>
2025-10-04 14:35:42 +0500

    shop-details adding the code

---
+++ resources/views/shop-details.blade.php
@@ -0,0 +1,288 @@
+@extends('layouts.app')
+@section('content')
+  <!-- Start Page Header -->
+  <section class="cs_page_heading cs_center cs_bg_filed" data-src="assets/img/page_heading_bg.jpg">
+    <div class="container">
+      <div class="cs_page_heading_in">
+        <h1 class="cs_page_heading_title cs_fs_48 cs_white_color">Shop Details</h1>
+        <ol class="breadcrumb cs_fs_20 cs_white_color cs_heading_font">
+          <li class="breadcrumb-item"><a href="index.html"><i class="fa-solid fa-house"></i>Home</a></li>
+          <li class="breadcrumb-item active">Shop Details</li>
+        </ol>
+      </div>
+    </div>
+  </section>
+  <!-- End Page Header -->
+  <!-- Start Product Details -->
+  <div class="cs_height_120 cs_height_lg_80"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="cs_single_product_thumb">
+          <div class="cs_single_product_thumb_item cs_radius_10">
+            <img src="assets/img/shop/product_thumb_lg_1.jpg" alt="Thumb">
+          </div>
+          <div class="cs_single_product_thumb_item cs_radius_10">
+            <img src="assets/img/shop/product_thumb_lg_2.jpg" alt="Thumb">
+          </div>
+          <div class="cs_single_product_thumb_item cs_radius_10">
+            <img src="assets/img/shop/product_thumb_lg_3.jpg" alt="Thumb">
+          </div>
+          <div class="cs_single_product_thumb_item cs_radius_10">
+            <img src="assets/img/shop/product_thumb_lg_4.jpg" alt="Thumb">
+          </div>
+          <div class="cs_single_product_thumb_item cs_radius_10">
+            <img src="assets/img/shop/product_thumb_lg_5.jpg" alt="Thumb">
+          </div>
+        </div>
+        <div class="cs_single_product_nav">
+          <div class="cs_single_product_thumb_mini cs_radius_10">
+            <img src="assets/img/shop/product_thumb_sm_1.jpg" alt="Thumb">
+          </div>
+          <div class="cs_single_product_thumb_mini cs_radius_10">
+            <img src="assets/img/shop/product_thumb_sm_2.jpg" alt="Thumb">
+          </div>
+          <div class="cs_single_product_thumb_mini cs_radius_10">
+            <img src="assets/img/shop/product_thumb_sm_3.jpg" alt="Thumb">
+          </div>
+          <div class="cs_single_product_thumb_mini cs_radius_10">
+            <img src="assets/img/shop/product_thumb_sm_4.jpg" alt="Thumb">
+          </div>
+          <div class="cs_single_product_thumb_mini cs_radius_10">
+            <img src="assets/img/shop/product_thumb_sm_5.jpg" alt="Thumb">
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="cs_single_product_details">
+          <h2>Single Diabetics Machine</h2>
+          <div class="cs_single_product_price_review">
+            <div class="cs_single_product_price cs_fs_30 cs_accent_color cs_semibold cs_heading_font">$20.00</div>
+            <div class="cs_client_rating">
+              <div class="cs_rating_container">
+                <div class="cs_rating" data-rating="5">
+                  <div class="cs_rating_percentage"></div>
+                </div>
+              </div>
+              <div class="cs_rating_text cs_fs_18 cs_heading_color">(2) Customer Revices</div>
+            </div>
+          </div>
+          <div class="cs_single_product_details_text cs_mb_35">
+            <p>Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud. Wiusmod tempor incididunt incididunt ut labore et dolore magna.</p>
+          </div>
+          <div class="cs_height_60 cs_height_lg_40"></div>
+          <div class="cs_quantity_and_btn">
+            <div class="cs_quantity cs_gray_bg cs_radius_100">
+              <button class="cs_quantity_button cs_accent_bg cs_center cs_radius_50 cs_decrement"><i
+                  class="fa-solid fa-minus"></i></button>
+              <span class="cs_quantity_input">1</span>
+              <button class="cs_quantity_button cs_accent_bg cs_center cs_radius_50 cs_increment"><i
+                  class="fa-solid fa-plus"></i></button>
+            </div>
+            <a href="#" class="cs_btn cs_style_1 cs_accent_bg cs_semibold cs_radius_100 text-uppercase"><span
+                class="cs_btn_text"><i class="fa-solid fa-bag-shopping"></i> Add to cart</span></a>
+          </div>
+          <div class="cs_height_40 cs_height_lg_40"></div>
+          <ul class="cs_single_product_info cs_fs_18">
+            <li><b>Availability: </b>20 left in Stock</li>
+            <li><b>SKU: </b>09809D-8</li>
+            <li><b>Categories: </b>Medicine Equipment</li>
+            <li><b>Tags: </b>Medical</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="cs_height_100 cs_height_lg_60"></div>
+    <div class="cs_product_meta_info">
+      <div class="cs_tabs cs_style1">
+        <ul class="cs_tab_links cs_product_tab cs_primary_font cs_semi_bold">
+          <li><a href="#tab_1">Description</a></li>
+          <li><a href="#tab_2">Additional information</a></li>
+          <li class="active"><a href="#tab_3">Review (1)</a></li>
+        </ul>
+        <div class="cs_height_40 cs_height_lg_40"></div>
+        <div class="cs_tab_body">
+          <div class="cs_tab" id="tab_1">
+            <p>Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud. Wiusmod tempor incididunt incididunt ut labore et dolore magna. Content marketing is
+              one of the most valuable strategies for increasing traffic to your site, earning more qualified leads, and
+              boosting conversions. But if you want to have an effective content marketing strategy, you need to keep it
+              up to date with with the latest trends. When you think of creating content, most people’s minds go
+              straight to blogging. While blogging is one of</p>
+            <p class="mb-0">consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+              magna aliquyam
+              erat diam voluptua accusam aolores duo et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+              est. In spite of disturbing the agile development process of the
+              DevOps approach security inputs only enhance the safeguards and reliability of the project. So, in a way,
+              DevSecOps will only impact on your audience</p>
+          </div><!-- .cs_tab -->
+          <div class="cs_tab" id="tab_2">
+            <table class="m-0">
+              <tbody>
+                <tr>
+                  <td>Color</td>
+                  <td>Blue, Gray, Green, Red, Yellow</td>
+                </tr>
+                <tr>
+                  <td>Size</td>
+                  <td>Large, Medium, Small</td>
+                </tr>
+              </tbody>
+            </table>
+            <hr>
+          </div><!-- .cs_tab -->
+          <div class="cs_tab active" id="tab_3">
+            <ul class="cs_client_review_list">
+              <li>
+                <div class="cs_client_review">
+                  <div class="cs_review_media">
+                    <div class="cs_review_media_thumb">
+                      <img src="assets/img/shop/avatar.png" alt="Avatar">
+                    </div>
+                    <div class="cs_review_media_right">
+                      <div class="cs_rating_container">
+                        <div class="cs_rating" data-rating="5">
+                          <div class="cs_rating_percentage"></div>
+                        </div>
+                      </div>
+                      <p class="m-0 cs_primary_color cs_semibold">Zhon Abony</p>
+                    </div>
+                  </div>
+                  <div class="cs_review_posted_by">August 12, 2024</div>
+                  <div class="cs_review_text">I recently purchased the Arino T-shirts and I'm thoroughly impressed. The
+                    sound quality is exceptional, the wireless connectivity is seamless, and the noise cancellation
+                    technology is a standout feature. They're a bit pricey, but well worth the investment. Highly
+                    recommend.</div>
+                </div>
+              </li>
+            </ul>
+            <div class="cs_height_85 cs_height_lg_45"></div>
+            <p class="m-0">Your email address will not be published. Required fields are marked *</p>
+            <div class="cs_height_20 cs_height_lg_20"></div>
+            <div class="cs_input_rating_wrap">
+              <p>Your rating *</p>
+              <div class="cs_input_rating" data-rating="0">
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+              </div>
+            </div>
+            <div class="cs_height_20 cs_height_lg_20"></div>
+            <form class="row">
+              <div class="col-lg-12">
+                <textarea cols="30" rows="7" class="cs_shop_form_field" placeholder="Write your review *"></textarea>
+                <div class="cs_height_25 cs_height_lg_25"></div>
+              </div>
+              <div class="col-lg-6">
+                <input type="text" class="cs_shop_form_field" placeholder="Your name *">
+                <div class="cs_height_25 cs_height_lg_25"></div>
+              </div>
+              <div class="col-lg-6">
+                <input type="text" class="cs_shop_form_field" placeholder="Your email *">
+                <div class="cs_height_25 cs_height_lg_25"></div>
+              </div>
+              <div class="col-lg-12">
+                <div class="cs_height_15 cs_height_lg_15"></div>
+                <div class="form-check m-0">
+                  <input class="form_check_input" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form_check_label m-0" for="flexCheckDefault">
+                    By using this form you agree with the storage and handling of your data by this website. *
+                  </label>
+                </div>
+                <div class="cs_height_40 cs_height_lg_30"></div>
+                <button class="cs_btn cs_style_1 cs_accent_bg cs_fs_18 cs_semibold" type="submit">
+                  <span class="cs_btn_text"> Submit</span>
+                </button>
+              </div>
+            </form>
+          </div><!-- .cs_tab -->
+        </div><!-- .cs_tab_body -->
+      </div><!-- .cs_tabs -->
+    </div>
+  </div>
+  <div class="cs_height_120 cs_height_lg_80"></div>
+  <!-- End Product Details -->
+  <!-- Start Related Products Section -->
+  <section>
+    <div class="container">
+      <div class="cs_section_heading cs_style_1 cs_text_center">
+        <h2 class="cs_section_title cs_fs_48 mb-0">Related products</h2>
+      </div>
+      <div class="cs_height_50 cs_height_lg_40"></div>
+      <div class="row cs_row_gap_30 cs_gap_y_30">
+        <div class="col-lg-3 col-sm-6">
+          <div class="cs_product_card cs_style_1 cs_type_1">
+            <div class="cs_product_thumbnail cs_center cs_radius_10">
+              <img src="assets/img/shop/product_4.png" alt="Product">
+              <div class="cs_card_btns">
+                <a href="cart.html"><i class="fa-solid fa-cart-shopping"></i></a>
+                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                <a href="shop-details.html"><i class="fa-solid fa-eye"></i></a>
+              </div>
+            </div>
+            <div class="cs_product_info">
+              <h2 class="cs_product_title cs_fs_20 cs_semibold"><a href="shop-details.html">Thermometer</a></h2>
+              <p class="cs_product_price cs_fs_18"><s>$10.00</s> <span class="cs_medium cs_heading_color">$8.00</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="cs_product_card cs_style_1 cs_type_1">
+            <div class="cs_product_thumbnail cs_center cs_radius_10">
+              <img src="assets/img/shop/product_2.png" alt="Product">
+              <div class="cs_card_btns">
+                <a href="cart.html"><i class="fa-solid fa-cart-shopping"></i></a>
+                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                <a href="shop-details.html"><i class="fa-solid fa-eye"></i></a>
+              </div>
+            </div>
+            <div class="cs_product_info">
+              <h2 class="cs_product_title cs_fs_20 cs_semibold"><a href="shop-details.html">Contact Thermometer</a>
+              </h2>
+              <p class="cs_product_price cs_fs_18"><s>$900.00</s> <span
+                  class="cs_medium cs_heading_color">$800.00</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="cs_product_card cs_style_1 cs_type_1">
+            <div class="cs_product_thumbnail cs_center cs_radius_10">
+              <img src="assets/img/shop/product_5.png" alt="Product">
+              <div class="cs_card_btns">
+                <a href="cart.html"><i class="fa-solid fa-cart-shopping"></i></a>
+                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                <a href="shop-details.html"><i class="fa-solid fa-eye"></i></a>
+              </div>
+            </div>
+            <div class="cs_product_info">
+              <h2 class="cs_product_title cs_fs_20 cs_semibold"><a href="shop-details.html">Stethoscope</a></h2>
+              <p class="cs_product_price cs_fs_18"><s>$100.00</s> <span class="cs_medium cs_heading_color">$80.00</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="cs_product_card cs_style_1 cs_type_1">
+            <div class="cs_product_thumbnail cs_center cs_radius_10">
+              <img src="assets/img/shop/product_6.png" alt="Product">
+              <div class="cs_card_btns">
+                <a href="cart.html"><i class="fa-solid fa-cart-shopping"></i></a>
+                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                <a href="shop-details.html"><i class="fa-solid fa-eye"></i></a>
+              </div>
+            </div>
+            <div class="cs_product_info">
+              <h2 class="cs_product_title cs_fs_20 cs_semibold"><a href="shop-details.html">Corona Mask</a></h2>
+              <p class="cs_product_price cs_fs_18"><s>$200.00</s> <span
+                  class="cs_medium cs_heading_color">$180.00</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cs_height_120 cs_height_lg_80"></div>
+  </section>
+  <!-- End Related Products Section -->
+@endsection
