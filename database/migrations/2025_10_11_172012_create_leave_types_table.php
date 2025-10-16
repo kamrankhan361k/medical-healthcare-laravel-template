3152e6959b58d59031d31fb67ebc72a3e59a0dad
unknown <kamrankhan3.61k@gmail.com>
2025-10-16 21:24:58 +0500

    adding the table column

--- database/migrations/2025_10_11_172012_create_leave_types_table.php
+++ database/migrations/2025_10_11_172012_create_leave_types_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('leave_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('max_days')->default(0);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
