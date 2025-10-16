58d07c9328e9754c8a842cdfe17593c55f71d3f0
unknown <kamrankhan3.61k@gmail.com>
2025-10-16 21:15:38 +0500

    adding the table column

--- database/migrations/2025_10_11_171609_create_users_table.php
+++ database/migrations/2025_10_11_171609_create_users_table.php
@@ -1,27 +1,13 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
+public function up(): void
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-    }
-};
+    Schema::create('users', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('email')->unique();
+        $table->string('password');
+        $table->string('phone')->nullable();
+        $table->string('address')->nullable();
+        $table->rememberToken();
+        $table->timestamps();
+    });
+}
